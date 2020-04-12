6faddcf4f9e30ff7d69a13256fbaa69a4d7bcfe7
Ijas Np <ijas.prof@gmail.com>
2020-04-12 15:14:22 +0400

    Updates

---
+++ precache-manifest.e6abb9158dbccb2c039c5a8f03f0d0d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd73f8a2718bfe297c1b4946e744d58",
+    "url": "/chemicaljoseweb/index.html"
+  },
+  {
+    "revision": "54e91897bd84c7ce60c3",
+    "url": "/chemicaljoseweb/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a499419440a124ec7e73",
+    "url": "/chemicaljoseweb/static/js/2.9dd5e784.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chemicaljoseweb/static/js/2.9dd5e784.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54e91897bd84c7ce60c3",
+    "url": "/chemicaljoseweb/static/js/main.15b70a7c.chunk.js"
+  },
+  {
+    "revision": "67873ff03c5a0394cb5f",
+    "url": "/chemicaljoseweb/static/js/runtime-main.da17c87a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chemicaljoseweb/static/media/logo.5d5d9eef.svg"
+  }
+]);
